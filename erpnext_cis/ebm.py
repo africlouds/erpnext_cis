9871dcdb09c9cc3189e8f17f0ee93e341b76baf4
Aimable Rwema <arwema@gmail.com>
2016-08-22 11:11:37 -0700

    After integration with MQTT

--- erpnext_cis/ebm.py
+++ erpnext_cis/ebm.py
@@ -4,12 +4,11 @@ import string
 import random
 from frappe.utils.background_jobs import enqueue
 import subprocess
+import paho.mqtt.client as mqtt
 
 def certify(doc, method):
-	invoice = frappe.get_doc("Sales Invoice", doc.name)
-	invoice.receipt_number = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-	invoice.internal_data = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-	invoice.receipt_signature = "ssssssssssssssssssssssssssss"
-	invoice.receipt_signature = "ssssssssssssssssssssssssssss"
-	invoice.save()
-	frappe.db.commit()
+        send_client = mqtt.Client()
+        send_client.connect("192.168.56.1", 1883, 60)
+        send_client.publish("SDC", "Certify %s" % doc.name)
+        send_client.loop(2)
+        send_client.disconnect()
